8e2a2a3bf3e67d5843b2f6def58d8a40eac2a345
dev server <info@pixolution.de>
2022-09-15 19:02:15 +0200

    Fix missing inference

--- python/main.py
+++ python/main.py
@@ -1,3 +1,5 @@
+import os
+os.environ['TF_XLA_FLAGS'] = '--tf_xla_auto_jit=-1'
 import tensorflow as tf
 import tensorflow_hub as hub
 from timeit import default_timer as timer
@@ -8,5 +10,7 @@ inference_func = hub.KerasLayer(model_path)
 # do warm up query
 start = timer()
 dummy_input = tf.constant(0.5, shape=(1, 224, 224, 3))
+inference_func(dummy_input)
 elapsed_seconds = timer() - start
-print(f"Python: warm up finished in {elapsed_seconds} seconds")
+print(f"Python: warm up without flags finished in {elapsed_seconds} seconds")
+
